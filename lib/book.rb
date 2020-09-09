65a44ac945d9c2f52ca19996a109ae5d2998b259
Jevoughn Wisdom <jevoughnwisdom@gmail.com>
2020-09-09 16:23:21 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,6 @@
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :author, :page_count,
+  attr_reader :title, :genre
 
   GENRES = []
 
@@ -14,5 +14,6 @@ class Book
 
   def genre=(genre)
     @genre = genre
+    GENRES << genre
   end
 end
